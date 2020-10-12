d05a092e23f61ca50f5a95587746503c8c86ab04
Victor Rodriguez <dev@chronometreur.me>
2020-10-11 21:57:08 -0500

    SingletonMeta

--- app/src/model/app.py
+++ app/src/model/app.py
@@ -4,6 +4,7 @@ Este buena practica la encontre en https://refactoring.guru/es/design-patterns/s
 """
 from threading import Lock, Thread
 
+
 # Implementacion del singleton con el metodo de la meta clase. Queda por
 # descubrir por que el singleton ya no se usa en python
 class SingletonMeta(type):
@@ -11,5 +12,19 @@ class SingletonMeta(type):
     # objeto lock que se usara para sincronizar los hilos cuando se llame al singleton
     _lock: Lock = Lock()
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def __call__(cls, *args, **kwargs):
+        # imagina que el programa recien arranca. Como todavia no hay instancia en el singleton, varios hilos pueden
+        # llegar a este punto casi al mismo tiempo. El primero entrara y bloqueara y el resto tendran que esperar
+        with cls._lock:
+            # el primer hilo entra y crea la instancia singleton. Al terminar, otro posible hilo que este esperando
+            # entra a esta seccion pero como el singleton ya esta inicializado no se creara un nuevo objeto
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+
+        return cls._instances[cls]
+
+
+# mi clase que contendra mi configuracion
+class Config(metaclass=SingletonMeta):
+    pass
